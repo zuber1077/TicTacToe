470d3420e968a958c6a9c6106eb15a98e2e9a070
zuber1077 <zuber1077@yahoo.com>
2019-11-19 07:53:30 +0800

    generate posiblites for expand square, ui improvment..

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Tic-Tac-Toe Game with Timer and expand square
+> when the time is left loose a move
+
+> HTML+CSS+JS+VueJS
--- js/Game.js
+++ js/Game.js
@@ -7,16 +7,17 @@ class Game {
     this.currentTurn = Game.white; // white, black
     this.squares = new Array(9).fill().map(() => new Square());
     this.winner = null;
+    this.wh = 3;
+    this.possibilities = [];
   }
 
-
-  clearTimer(){
+  clearTimer() {
     clearInterval(g_timer);
     this.timer = 5;
   }
   makeMove(i) {
     this.clearTimer();
-    this.timerTick()
+    this.timerTick();
     // if no value
     if (this.inProgress && !this.squares[i].value) {
       this.squares[i].value = this.currentTurn;
@@ -25,60 +26,103 @@ class Game {
       this.checkForWinner();
 
       this.changePlayer();
-    } else {
-      // show reset button
     }
   }
 
- changePlayer() {
+  changePlayer() {
     this.currentTurn =
-        this.currentTurn === Game.white ? Game.black : Game.white;
+      this.currentTurn === Game.white ? Game.black : Game.white;
   }
 
   checkForWinner() {
-    var possibilities = [
-      [0, 4, 8],
-      [0, 3, 6],
-      [2, 4, 6],
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [2, 5, 6],
-      [1, 4, 7]
-    ];
+    if (this.moveMade > 2) {
+      for (let winPos of this.possibilities) {
+        let Xs = 0;
+        let Os = 0;
 
-    possibilities.forEach(ps => {
-      const [a, b, c] = ps;
-      const sqA = this.squares[a];
-      const sqB = this.squares[b];
-      const sqC = this.squares[c];
+        for (let posIndex of winPos) {
+          if (this.squares[posIndex].value === "x") {
+            Xs++;
+          } else if (this.squares[posIndex].value === "o") {
+            Os++;
+          }
+        }
 
-      if (sqA.value && sqA.value === sqB.value && sqA.value === sqC.value) {
-        this.inProgress = false; // finished
-        this.winner = sqA.value; // white or black
+        if (Xs === this.wh) {
+          alert("X won!");
+          this.restart();
+          break;
+        }
 
-        sqA.isHighlighted = sqB.isHighlighted = sqC.isHighlighted = true; // set all
-        this.clearTimer();
+        if (Os === this.wh) {
+          alert("O won!");
+          this.restart();
+          break;
+        }
       }
-    });
-
-    // check for tie
-    if (this.moveMade === this.squares.length) {
-      this.inProgress = false; // inProgress = false AND winner = null
-      this.clearTimer();
+      
+      // check for tie
+      if (this.moveMade === this.squares.length - 1) {
+        alert("Draw!");
+        this.restart();
+      }
     }
+  }
+
+  restart() {
+    this.inProgress = false;
+    this.squares = [];
+    this.possibilities = [];
+    this.clearTimer();
+    // this.wh = '';
+    this.moveMade = 0;
+    this.timer = "";
   }
 
   timerTick() {
     g_timer = setInterval(() => {
       this.timer--;
       // console.log(this.timer);
-      if(this.timer <= 0){
+      if (this.timer <= 0) {
         this.changePlayer();
         this.clearTimer();
         this.timerTick();
       }
     }, 1000);
+  }
+
+  makeSquares(num) {
+    return new Array(num).fill().map(() => new Square());
+  }
+
+  makePossibilities(userInput) {
+    let _possibilities = [];
+    let second = [];
+    let m = userInput;
+    let input = m * m - 1;
+    let num = 0;
+    // first
+    for (let i = 0; i < input; i += m) {
+      _possibilities.push(Array.from({ length: m }, (_, j) => (j += num)));
+      num += m;
+    }
+
+    // second
+    for (let i = 0; i < _possibilities.length; i++) {
+      second.push(
+        Array.from({ length: m }, (_, j) => j * _possibilities.length + i)
+      );
+    }
+
+    // merge
+    _possibilities.push(...second);
+
+    // third
+    _possibilities.push(Array.from({ length: m }, (_, j) => j * (m + 1)));
+
+    _possibilities.push(Array.from({ length: m }, (_, j) => (j + 1) * m - 1));
+
+    return _possibilities;
   }
 }
 
